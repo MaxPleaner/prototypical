ae1b118b56150ca75b11e95e3f222f8acb08ebd6
maxpleaner <maxpleaner@gmail.com>
2016-03-17 23:45:12 -0700

    remove turbolinks, update connection grud

--- plugins/online_plugin/config/routes.rb
+++ plugins/online_plugin/config/routes.rb
@@ -1,3 +1,11 @@
 OnlinePlugin::Engine.routes.draw do
-  resources :connections
+  get "/", to: "connections#index", as: :connections
+  post "/", to: "connections#create"
+  get "/new", to: "connections#new", as: :new_connection
+  get "/:id/edit", to: "connections#edit", as: :edit_connection
+  get "/:id", to: "connections#show", as: :connection
+  put "/:id", to: "connections#update"
+  patch "/:id", to: "connections#update"
+  delete "/:id", to: "connections#destroy"
+
 end
