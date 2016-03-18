68fdebcac12159204d2ece382e222a4f8b9272b1
maxpleaner <maxpleaner@gmail.com>
2016-03-17 22:51:54 -0700

    write CRUD routes for plugins; make scaffold for tutors

--- plugins/tutorials_plugin/config/routes.rb
+++ plugins/tutorials_plugin/config/routes.rb
@@ -1,2 +1,11 @@
 TutorialsPlugin::Engine.routes.draw do
+  get "/", to: "tutorials#index", as: :tutorials
+  post "/", to: "tutorials#create"
+  get "/new", to: "tutorials#new", as: :new_tutorial
+  get "/:id/edit", to: "tutorials#edit", as: :edit_tutorial
+  get "/:id", to: "tutorials#show", as: :tutorial
+  put "/:id", to: "tutorials#update"
+  patch "/:id", to: "tutorials#update"
+  delete "/:id", to: "tutorials#destroy"
+
 end
